79ae547d8ce173e555e6a159fe0c3d3f3c5e8d5e
Conor <conorboyle07@protonmail.com>
2026-03-06 10:55:51 +0000

    feat: Append name of algorithm to start

--- haskellorithm.hs
+++ haskellorithm.hs
@@ -1,8 +1,8 @@
 tperm :: [Char]
-tperm = "R U R' F' R' F R2 U' R' U' R U R' F'"
+tperm = "T: R U R' F' R' F R2 U' R' U' R U R' F'"
 
 aaperm :: [Char]
-aaperm = "x L2 D2 L' U' L D2 L' U L'"
+aaperm = "Aa: x L2 D2 L' U' L D2 L' U L'"
 
 main = do 
   putStrLn tperm
